10b0bbebb1bdad2b6030329610f10eec2be5ba6e
daniil.dedov@telekom.com <daniil.dedov@telekom.com>
2022-03-31 10:58:22 +0300

    w/o may

--- handler/rfc8693/flow_token_exchange.go
+++ handler/rfc8693/flow_token_exchange.go
@@ -28,8 +28,6 @@ type Handler struct {
 // HandleTokenEndpointRequest implements https://tools.ietf.org/html/rfc8693#section-2.1 (currently impersonation only)
 func (c *Handler) HandleTokenEndpointRequest(ctx context.Context, request fosite.AccessRequester) error {
 
-	log.Println("Flow_echange HandleTokenEndpointRequest")
-
 	// From https://tools.ietf.org/html/rfc8693#section-2.1:
 	//
 	//	grant_type
@@ -41,7 +39,6 @@ func (c *Handler) HandleTokenEndpointRequest(ctx context.Context, request fosite
 
 	client := request.GetClient()
 
-	log.Println(string(client.GetMetaData()))
 	//log.Println(string(request.GetSubjectTokenClient().GetMetaData()))
 	if client.IsPublic() {
 		return errors.WithStack(fosite.ErrInvalidGrant.WithHint("The OAuth 2.0 Client is marked as public and is thus not allowed to use authorization grant \"urn:ietf:params:oauth:grant-type:token-exchange\"."))
@@ -78,28 +75,6 @@ func (c *Handler) HandleTokenEndpointRequest(ctx context.Context, request fosite
 		return errors.WithStack(fosite.ErrInvalidRequest.WithHintf("Currently only \"subject_token_type=urn:ietf:params:oauth:token-type:access_token\" is supported but got \"%s\".", subjectTokenType))
 	}
 
-	//// From https://tools.ietf.org/html/rfc8693#section-2.1:
-	////
-	////	actor_token
-	////		OPTIONAL . A security token that represents the identity of the acting party.
-	////		Typically, this will be the party that is authorized to use the requested security
-	////		token and act on behalf of the subject.
-	//actorToken := form.Get("actor_token")
-	//if actorToken != "" {
-	//	return errors.WithStack(fosite.ErrInvalidRequest.WithHintf("\"actor_token\" was provided but delegation is currently not supported."))
-	//}
-	//
-	//// From https://tools.ietf.org/html/rfc8693#section-2.1:
-	////
-	////	actor_token_type
-	////		An identifier, as described in Section 3, that indicates the type of the security token
-	////		in the actor_token parameter. This is REQUIRED when the actor_token parameter is present
-	////		in the request but MUST NOT be included otherwise.
-	//actorTokenType := form.Get("actor_token_type")
-	//if actorTokenType != "" {
-	//	return errors.WithStack(fosite.ErrInvalidRequest.WithHintf("\"actor_token_type\" was provided but delegation is currently not supported."))
-	//}
-
 	sig := c.CoreStrategy.AccessTokenSignature(subjectToken)
 	or, err := c.CoreStorage.GetAccessTokenSession(ctx, sig, request.GetSession())
 	if err != nil {
@@ -108,6 +83,7 @@ func (c *Handler) HandleTokenEndpointRequest(ctx context.Context, request fosite
 		return err
 	}
 
+	//TODO
 	var subjectTokenClientId string
 	if or.GetSubjectTokenClient() == nil {
 		// first exchange request has no subjects token client set
@@ -132,8 +108,6 @@ func (c *Handler) HandleTokenEndpointRequest(ctx context.Context, request fosite
 		return errors.WithStack(fosite.ErrUnauthorizedClient.WithHint("The OAuth 2.0 Client is not allowed to perform a token exchange for the given subject token."))
 	}
 
-	request.GetClient().SetMetaData([]byte("bossik"))
-
 	tokenExchangeRequest, ok := request.(fosite.TokenExchangeAccessRequester)
 	if !ok {
 		return errors.WithStack(fosite.ErrInvalidRequestObject)
@@ -157,6 +131,12 @@ func (c *Handler) HandleTokenEndpointRequest(ctx context.Context, request fosite
 	if c.RefreshTokenLifespan > -1 {
 		request.GetSession().SetExpiresAt(fosite.RefreshToken, time.Now().UTC().Add(c.RefreshTokenLifespan).Round(time.Second))
 	}
+
+	errUpd := c.Store.UpdateClient(ctx, &client)
+	if errUpd != nil {
+		log.Println("Update Error ")
+	}
+
 	return nil
 }
 
